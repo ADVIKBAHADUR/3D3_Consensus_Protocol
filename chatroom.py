fa752ee04ecd7645d77629bed7e6a34d17619387
Enzo Moser <irlenzom@tcd.ie>
2024-04-03 21:52:31 +0500

    Added commands. Cleaned cli. Implementing ACK. WARNING - NOT TESTED

--- chatroom.py
+++ chatroom.py
@@ -7,35 +7,110 @@ import sys
 node_list = []  # global Variable to hold the IP addresses of the nodes in the group
 
 #self_ip_addr = socket.gethostbyname(socket.gethostname())
-self_ip_addr = '0.0.0.0'
-srvr_ip_addr = '192.168.24.71'
-print(f"Self IP Address= {self_ip_addr}")
+self_ip_addr = '0.0.0.0'    # Does NOT need changing
+srvr_ip_addr = '0.0.0.0'    # Does need changing
 srvr_tcp_port = 5090
 self_tcp_port = 5091
 udp_port = 5190
 snd_udp_port = 5188
 srvr_con_sts = "Server is Down"
+your_prompt = "(You) >>> "
+
+UP = '\033[1A'
+CLEAR = '\x1b[2K'
+CLEAR_SCREEN = '\u001Bc'
+
+sent_ips = 0
+ACKED_ips = 0
+wait_for_ACK = False
+
+ACK_char = 'ơ̴̧̧̨̧̮̩͖̮͙͉͇͖̳͓̣̰̥͔͈̙͚̥̭͖͍̭͖̙̼̰͖̗̗̮̳͎̤̘̱̠͔̄̅̎̊̉͐̓̚͜͜ͅͅ'
+
+def get_ip():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        try:
+            # does not need to be reachable
+            s.connect(('10.254.254.254', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
 
 # a. Send Broadcast UDP Message from Server to All Nodes to update the IP List
 # b. Send CHAT Message in the group 
 def send_udp_msg(udp_sock, msg):
     global node_list
+    global sent_ips
+    global ACKED_ips
+    global wait_for_ACK
+    wait_for_ACK = True
     serialised_data = pickle.dumps(msg)
-
-    print(f"Nodes available to send the message : send_udp_msg {node_list}")
+    #print(pickle.dumps(msg))
     for ip_addr in node_list: 
       if(ip_addr != self_ip_addr):
         try:
           udp_sock.sendto(serialised_data,(ip_addr, udp_port))
         except socket.error as err:
           print(f"Could Not Send message to {ip_addr}: send_udp_msg  : {err}")
-    
+        else:
+          sent_ips += 1
+    start_time = time.clock_gettime_ns(1)
+    #print(start_time)
+    time_diff = 0
+    max_time_diff = 12*(10**9)
+    while (time_diff < max_time_diff):
+        time_diff = time.clock_gettime_ns(1) - start_time
+        if (ACKED_ips >= sent_ips):
+            return
+        time.sleep(1)
+    max_time = max_time_diff /  (10**9)
+    missing_users = sent_ips - ACKED_ips
+    print(f"Over {max_time} seconds has passed. {missing_users} out of {sent_ips} users were not reached")
+    wait_for_ACK = False
+    ACKED_ips = 0
+    sent_ips = 0
+
+# Send a command to client
+def send_command(send_msg_sock,msg):
+    match msg.lower():
+        case "/help":
+            print(f"""\t/help - This Help Menu
+\t/thread - Print Total Client Threads
+\t/status - Print Server Status
+\t/userlist - Show the list of users (nodes) connected""")
+        case "/thread":
+            print_client_threads()
+        case "/threads":
+            print_client_threads()
+        case "/status":
+            print_server_status()
+        case "/userlist":
+            print_userlist()
+        case default:
+            print(f"Invalid command. Type \"\help\" ")
+
+#Repeated print functions
+def print_client_threads():
+    print(f"Total Client Thread : {threading.active_count()}")
+
+def print_server_status():
+    global srvr_con_sts
+    global srvr_ip_addr
+    print(f"{srvr_con_sts} At {srvr_ip_addr}")
+
+def print_userlist():
+    global node_list
+    print(f"Nodes available to send the message : {node_list}")    
 
 # TCP Server - Sub function - Active Connection Thread - Sends the Updated Node(IP) List
 # to all Nodes in the group in UDP message
 def start_conn_thread(conn,bc_sock,addr):
     global node_list
     print(f"Client Node Connected at Server : {addr}")
+    print(f"Connection is: {conn}")
     if addr[0] not in node_list:
       node_list.append(addr[0])
       send_udp_msg(bc_sock,node_list)
@@ -46,7 +121,8 @@ def start_conn_thread(conn,bc_sock,addr):
         if not msg:
           conn.close()
           print(f"Server Side No msf recv Error occurred")
-          node_list.remove(addr[0])
+          if (addr[0]):
+            node_list.remove(addr[0])
           send_udp_msg(bc_sock,node_list)
           print(f"Node {addr} removed from Node List \nNow Updated Node List at Server {node_list}")
           break
@@ -91,23 +167,23 @@ def start_conn():
         conn_sock.bind((self_ip_addr, self_tcp_port))
         try:
           conn_sock.connect((srvr_ip_addr, srvr_tcp_port))
-          srvr_con_sts = "Server is Up" 
+          srvr_con_sts = "Server is UP" 
           while True:
             msg = conn_sock.recv(1024)
             try:
               if not msg:
                 conn_sock.close()
-                srvr_con_sts = "Server is Down"
+                srvr_con_sts = "Server is DOWN"
                 print(f"Server Down detected at Client - Reconnecting")
                 break
             except socket.error as err:
               conn_sock.close()
-              srvr_con_sts = "Server is Down"
+              srvr_con_sts = "Server is DOWN"
               print(f"Server Down exception detected at Client - Reconnecting")
               break
         except socket.error as err:
 #           print(f"Socket Connect Error : {err}")
-            srvr_con_sts = "Server is Down"
+            srvr_con_sts = "Server is DOWN"
             conn_sock.close()
 
 
@@ -115,6 +191,11 @@ def start_conn():
 # from others nodes in the group
 def start_conn_upd():
     global node_list
+    global UP
+    global CLEAR
+    global ACKED_ips
+    global ACK_char
+    global wait_for_ACK
 
     while True:
         conn_upd_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -123,6 +204,7 @@ def start_conn_upd():
         while True:
           msg, addr = conn_upd_sock.recvfrom(5020)
           try:
+            print(UP, end = CLEAR)
             if not msg:
               conn_upd_sock.close()
               print(f"Socket error detected at start_conn_upd - Reconnecting")
@@ -130,58 +212,80 @@ def start_conn_upd():
             else:
               if addr[0] == srvr_ip_addr:
                 node_list = pickle.loads(msg)
-                #print(f"Updated IP List = {node_list}")
-                print(f"Online Node List = {node_list}\n>>> ")
+                #print_userlist()
               else:
                 dmsg = pickle.loads(msg)
                 if dmsg != '' and dmsg != '\n': 
-                #print(f"\nChat Message from {addr[0]} :: {pickle.loads(msg)}\n>>> ")
-                  print(f"Chat Message from {addr[0]} :: {dmsg}\n>>> ")
-
+                    if dsmeg != ACK_char:
+                        udp_sock.sendto(pickle.dumps(ACK_char),(addr, udp_port))
+                        print(f"{addr[0]} | {dmsg}")
+                        print(your_prompt, end = "")
+                    else:
+                        if (wait_for_ACK):
+                            ACKED_ips += 1
           except socket.error as err:
             conn_upd_sock.close()
             print(f"Socket exception detected at start_conn_upd - Reconnecting")
             break
 
-
 def main():
     global node_list
     global srvr_con_sts
+    global your_prompt
+    print("", end = CLEAR_SCREEN)
+    self_ip_addr = get_ip()
+    print(f"\n\t--- Your IP Address is: {self_ip_addr} ---\n")
+    choice = input("Type \"1\" for SERVER. Or press any key for CLIENT\n>>> ")
 
-    choice = input("Enter Node Type Server or Client : ")
-
-    if choice.upper() == "SERVER" :
-        print("Create Server Thread")
+    if choice == "1" :
+        print("Creating Server Thread...", end='')
         srvr_thread = threading.Thread(target=start_server,)
         srvr_thread.daemon = True
         srvr_thread.start()
+        print(" DONE!")
         while True:
           print(f"Total Client Thread : {threading.active_count()}")
           msg = input()
     else:
-        print("Create Client Thread")
+        nickname = input("Enter you nickname >>> ")
+        while(len(nickname) > 10):
+            nickname = input("The nickname must be less than 10 characters >>> ")
+        your_prompt = nickname + your_prompt
+
+        print("Creating Client Thread...", end='')
         conn_thread = threading.Thread(target=start_conn,)
         conn_thread.daemon = True
         conn_thread.start()
+        print(" DONE!")
 
-        print("Create Conn Update Thread at Client")
+        print("Creating Update Thread...", end='')
         conn_upd_thread = threading.Thread(target=start_conn_upd,)
         conn_upd_thread.daemon = True
         conn_upd_thread.start()
+        print(" DONE!")
+
+        print_client_threads()
+        print_server_status()
+
+        print("Type \"\help\" for a list of commands...\n\n")
 
         while True:
             send_msg_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             send_msg_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             send_msg_sock.bind((self_ip_addr, snd_udp_port))
-            print(f"Total Client Thread : {threading.active_count()}")
-            print(f"{srvr_con_sts}\n>>> ")
-#           print(f"Online Nodes :: {node_list}")
-#           msg = input("Enter Message :")
-            msg = input()
-            if msg != '': 
-              send_udp_msg(send_msg_sock,msg)
-            print(f"Online Nodes :: {node_list}")
-
+            msg = input(your_prompt)
+            while(len(msg) > 100):
+                msg = input("The msg must be less than 100 characters >>> ")
+            if msg != '':
+              if msg[0] == '/':
+                send_command(send_msg_sock,msg)
+              else:
+                if node_list == []:
+                    print(UP, end = CLEAR)
+                    print(f"{your_prompt}{msg} ....... No other users connected...")
+                else:
+                    msg = "{nickname} : {msg}"
+                    send_udp_msg(send_msg_sock,msg)
 try:
   main()
 except KeyboardInterrupt:
